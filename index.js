1dc5680ada761c6781e8a7e41c519e59f53c9c61
saiffardin <saiffardin@gmail.com>
2020-09-30 19:31:38 +0600

    CRUD done - life sucks || onk basic bujha lagbe

--- index.js
+++ index.js
@@ -1,41 +1,110 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const password = '7tk93l4yEI9dWFL2';
 
 // ---------------------------------------------------------------- version 2.2.12
 
 const MongoClient = require('mongodb').MongoClient;
+
+const ObjectID = require('mongodb').ObjectID; // id dhore delete korar jonno
+
 const uri = "mongodb://jm-module-48:7tk93l4yEI9dWFL2@cluster0-shard-00-00.opwzi.mongodb.net:27017,cluster0-shard-00-01.opwzi.mongodb.net:27017,cluster0-shard-00-02.opwzi.mongodb.net:27017/organicdb?ssl=true&replicaSet=atlas-t6aucd-shard-0&authSource=admin&retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 // ----------------------------------------------------------------
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false })); // form k properly parse korar jonno
+
 
 app.get('/', (req, res) => {
     console.log('I am working on IDE console');
-    res.send('I am working on local host');
+    // res.send('I am working on local host');
+    res.sendFile(__dirname + '/index.html');
 })
+
+
 
 // ---------------------------------------------------------------- new connect
 
+
 client.connect(err => {
-    const collection = client.db("organicdb").collection("products");
+    const productCollection = client.db("organicdb").collection("products");
 
     // perform actions on the collection object
     console.log('database connected');
 
-    const product = { name: "honey", price: 25, quantity: 40 };
 
-    collection.insertOne(product)
-        .then(() => {
-            console.log('product added successfully.');
+    // ---------------------------------------------------------------- below Add product into database
+    app.post("/addProduct", (req, res) => {
+        const product = req.body;
+        console.log(product);
+
+        productCollection.insertOne(product)
+            .then(() => {
+                console.log('Data added successfully');
+                res.send('success');
+            })
+    })
+
+
+
+    // ---------------------------------------------------------------- below READ from database
+    app.get('/products', (req, res) => {
+        console.log('I am in products - IDE console');
+        // res.send('I am in products - localhost');
+
+        productCollection.find({})
+            .toArray((err, docs) => {
+                res.send(docs);
+            })
+    })
+
+
+    // ---------------------------------------------------------------- below DELETE from database
+    app.delete('/delete/:id', (req, res) => {
+        console.log(req.params);
+        productCollection.deleteOne({
+            _id: ObjectID(req.params.id)
+        }).then(res => {
+            console.log(res);
         })
+    })
 
 
-    // client.close();
+    // ---------------------------------------------------------------- below Update a single product in database
+
+    // first LOAD a single product
+    app.get('/product/:id', (req, res) => {
+        productCollection.find({ _id: ObjectID(req.params.id) })
+            .toArray ((err,docs) => {
+                res.send(docs[0]);
+            })
+    })
+
+
+    // UPDATE that product
+    app.patch('/update/:id', (req, res) => {
+        console.log(req.body.price);
+        productCollection.updateOne(
+            { _id: ObjectID(req.params.id)},
+            {
+                $set : {
+                    price: req.body.price,
+                    quantity: req.body.quantity
+                }
+            }
+        ) 
+        .then(result => {
+            console.log(result)
+        })
+    })
+
 });
 
-// ---------------------------------------------------------------- add a single product
+
 
 
 
